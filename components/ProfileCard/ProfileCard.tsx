24bf2d6c1abc6bd5a11696fb3aa46ec62821a34e
Yakumo-zi <1477717283@qq.com>
2024-08-24 22:31:30 +0800

    feat:change most used language be a picture

--- components/ProfileCard/ProfileCard.tsx
+++ components/ProfileCard/ProfileCard.tsx
@@ -4,11 +4,9 @@ import Image from "next/image";
 import Link from "next/link";
 import { ComponentProps } from "react";
 import Tag from "../UI/Tag";
+import MostUesdLanguages from "@/public/most_used_language.svg";
 
 type Props = {} & ComponentProps<"div">;
-const mostLanguagesPicture =
-  "https://camo.githubusercontent.com/a4d5172a859ec46e1a15291e1f382ca6ba10a49cd0e059d774d66001cf2c605e/68747470733a2f2f6769746875622d726561646d652d73746174732d696f74612d73616e64792d32342e76657263656c2e6170702f6170692f746f702d6c616e67732f3f757365726e616d653d59616b756d6f2d7a69266c61796f75743d636f6d706163742673697a655f7765696768743d302e3526636f756e745f7765696768743d302e35266578636c7564655f7265706f3d6769746875622d726561646d652d73746174732c6d63755f73747564792c646f7466696c65732c6275696c642d796f75722d6f776e2d782c6e6f74652c7275737426686964653d4d616b6566696c652c48544d4c2c417373656d626c792c5368656c6c2c434d616b65";
-
 const ProfileCard = ({ className, ...props }: Props) => {
   return (
     <div
@@ -46,12 +44,10 @@ const ProfileCard = ({ className, ...props }: Props) => {
         </span>
       </div>
       <Image
-        src={mostLanguagesPicture}
+        src={MostUesdLanguages}
         alt="profile"
         width={244}
         height={144}
-        unoptimized
-        priority
       />
     </div>
   );
